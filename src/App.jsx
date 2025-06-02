da786909518d706962319ecfa227ce0c53c187a9
Challa Rishi <challarishi02@gmail.com>
2025-06-02 21:26:48 +0530

    change in main and app

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,25 @@ import { useChatStore } from "./lib/chatStore";
 
 const App = () => {
 
-  const {currentUser, isLoading, fetchUserInfo} = useUserStore();
+  const { currentUser, isLoading, fetchUserInfo } = useUserStore();
   const { chatId } = useChatStore();
 
   useEffect(() => {
     const unSub = onAuthStateChanged(auth, (user) => {
-      fetchUserInfo(user?.uid);
+      if (user) {
+        // User is signed in
+        fetchUserInfo(user.uid);
+      } else {
+        // User is signed out - clear the user store
+        useUserStore.setState({ currentUser: null, isLoading: false });
+        // Also clear chat store when user logs out
+        useChatStore.setState({ 
+          chatId: null, 
+          user: null, 
+          isCurrentUserBlocked: false, 
+          isReceiverBlocked: false 
+        });
+      }
     });
 
     return () => {
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
@@ -6,22 +6,29 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from './lib/firebase.js';
 import { useUserStore } from './lib/userStore.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
-
-useEffect(() => {
+// Create a wrapper component for authentication
+const AuthProvider = ({ children }) => {
+  useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
-        if (user) {
-            // User is signed in
-            useUserStore.getState().fetchUserInfo(user.uid);
-        } else {
-            // User is signed out
-            useUserStore.getState().set({ currentUser: null, isLoading: false });
-        }
+      if (user) {
+        // User is signed in
+        useUserStore.getState().fetchUserInfo(user.uid);
+      } else {
+        // User is signed out
+        useUserStore.setState({ currentUser: null, isLoading: false });
+      }
     });
 
     return () => unsubscribe();
-}, []);
+  }, []);
+
+  return children;
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+)
